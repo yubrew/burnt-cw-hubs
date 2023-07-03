7a592f10c232cfb8084b26b9a36fcdc81fc91f43
peartes <kenny.fale.kf@gmail.com>
2023-07-03 22:58:05 +0100

    chore: remove unneeded states

--- seat/src/state.rs
+++ seat/src/state.rs
@@ -23,20 +23,6 @@ pub struct Config {
     pub owner: Addr,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
-pub struct ContractVersion {
-    /// contract is a globally unique identifier for the contract.
-    /// it should build off standard namespacing for whichever language it is in,
-    /// and prefix it with the registry we use.
-    /// For rust we prefix with `crates.io:`, to give us eg. `crates.io:cw20-base`
-    pub contract: String,
-    /// version is any string that this implementation knows. It may be simple counter "1", "2".
-    /// or semantic version on release tags "v0.6.2", or some custom feature flag list.
-    /// the only code that needs to understand the version parsing is code that knows how to
-    /// migrate from the given contract (and is tied to it's implementation somehow)
-    pub version: String,
-}
-
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
 pub struct SeatMetadata {
     pub name: String,
@@ -293,4 +279,3 @@ impl<'a> SeatModules<'a, SeatMetadata, TokenMetadata> {
     }
 }
 pub const CONFIG: Item<Config> = Item::new("config");
-pub const CONTRACT_INFO: Item<ContractVersion> = Item::new("contract_info");
