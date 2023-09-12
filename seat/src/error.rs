62cc8f89bd58ae422958fead351ff794f103d4f4
peartes <kenny.fale.kf@gmail.com>
2023-09-12 23:33:42 +0100

    chore: make sure error display shows source error

--- seat/src/error.rs
+++ seat/src/error.rs
@@ -6,18 +6,18 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("Ownable Module Error")]
+    #[error("Ownable Module Error {0}")]
     OwnableError(#[from] ownable::OwnableError),
 
-    #[error("Metadata Module Error")]
+    #[error("Metadata Module Error {0}")]
     MetadataError(#[from] metadata::MetadataError),
 
-    #[error("Token Module Error")]
+    #[error("Token Module Error {0}")]
     SeatTokenError(#[from] cw721_base::ContractError),
 
-    #[error(transparent)]
+    #[error("Sellable Module Error {0}")]
     SellableError(#[from] sellable::errors::ContractError),
 
-    #[error("Sales Module Error")]
+    #[error("Sales Module Error {0}")]
     SalesError(#[from] sales::errors::ContractError),
 }
